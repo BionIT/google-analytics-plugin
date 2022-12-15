c18a1f8d44187c32f0a189d678f31c5b1c9ab736
Lu Yu <nluyu@amazon.com>
2022-12-15 08:44:39 -0800

    Create scheduled workflow to check recent release of OSD and create PR (#9)
    * test first githubaction to fetch recent release
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * start workflow when pushing change to dev branch
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * test workflow template and checkout
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * fix format
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * fix reusable workflow syntax
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * fix wf indent
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * change branch
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * fix var name
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * test yarn build and set up node action
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * fix syntax
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * use git command instead
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * use checkout again
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * run yarn bootstrap
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * fix indentation
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * add script to bump version and add workflow to create pr
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * removed unused logic
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    * use PAT
    
    * add input
    
    * fix indent
    
    * remove type for secret
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>
    
    Signed-off-by: Lu Yu <nluyu@amazon.com>

---
+++ bin/bump_version.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import json
+import os
+import sys
+
+def run():
+    if len(sys.argv) < 2:
+        print("No version is specified, do nothing")
+        return
+    args = sys.argv[1]
+    curr_dir = os.path.dirname(os.path.abspath(__file__))
+    new_package_json = f"{curr_dir}/../package.json"
+    with open(new_package_json, "r") as f:
+        data = json.load(f)
+        data["version"] = f'{args}.0'
+    os.remove(new_package_json)
+    with open(new_package_json, "w") as f:
+        json.dump(data, f, indent=4)
+
+    new_osd_json = f"{curr_dir}/../opensearch_dashboards.json"
+    with open(new_osd_json, "r") as f:
+        data = json.load(f)
+        data["version"] = f'{args}.0'
+        data["opensearchDashboardsVersion"] = f'{args}'
+    os.remove(new_osd_json)
+    with open(new_osd_json, "w") as f:
+        json.dump(data, f, indent = 4)
+
+if __name__ == "__main__":
+    run()
